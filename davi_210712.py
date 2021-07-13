c966495e5c1a113dce7aa38d34dd730d731732a7
davicova <86981409+davicova@users.noreply.github.com>
2021-07-12 22:22:39 -0300

    Add files via upload

---
+++ davi_210712.py
@@ -0,0 +1,18 @@
+cars=["Mercedes","Honda","Toiota","Bmw","Audi","Fiat"]
+print(cars[1])
+cars.insert(0,"bicycle")
+print(cars)
+cars[0]="Davi"
+print(cars)
+print(cars[0]+" "+cars[1])
+print(cars[6].upper())
+cars.insert(2,"Sumare")
+print(cars)
+del cars[1]
+print(cars)
+comida=cars.pop(6)
+print(comida)
+comida=cars.pop(0)
+print(comida)
+
+
